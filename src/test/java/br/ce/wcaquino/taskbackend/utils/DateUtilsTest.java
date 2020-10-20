2098dd37fa08c4d768dcc20e8488c40f4fdf2e7c
cristovaopbgyn <cristovaopbalfa@hotmail.com>
2020-10-20 16:49:55 -0300

    testes unitários

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,29 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DateUtilsTest {
+	
+	@Test
+	public void retornaTrueParaDataFutura() {
+		LocalDate data = LocalDate.of(2090, 1, 1);
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(data));
+	}
+	
+	@Test
+	public void retornaFalseParaDataPassada() {
+		LocalDate data = LocalDate.of(2014, 1, 1);
+		Assert.assertFalse(DateUtils.isEqualOrFutureDate(data));
+	}
+	
+	@Test
+	public void retornaTrueParaDataPresente() {
+		LocalDate data = LocalDate.now();
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(data));
+		
+		System.out.println("teste");
+	}
+}
